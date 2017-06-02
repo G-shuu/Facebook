b6bb9392121cfc9fa1a559ac8b29ce8a421ca753
Your Name <you@example.com>
2017-06-02 23:18:46 +0900

    fog

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -9,6 +9,28 @@ class AvatarUploader < CarrierWave::Uploader::Base
   storage :fog
 
   process :resize_to_limit => [50, 150]
+
+  version :thumb do
+    process :crop
+    process :resize_to_limit => [100, 100]
+  end
+
+  def crop
+    manipulate! do |img|
+      gravity = Magick::CenterGravity # 中央から切り取ります。
+      crop_w = img.columns
+      crop_h = img.rows
+      # 画像のサイズが縦横違った場合は小さい方に合わせてトリミングする。
+      if img.rows <= img.columns
+        crop_w = img.rows
+      else
+        crop_h = img.columns
+      end
+      img.crop!(gravity, crop_w, crop_h)
+      img = yield(img) if block_given?
+      img
+    end  
+  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
